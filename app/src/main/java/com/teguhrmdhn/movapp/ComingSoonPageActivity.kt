6af053f03d261e5d880441d29a7842fe0a7dc2ef
teguh ramadhan <teguhrmdhn@gmail.com>
2020-09-22 20:20:05 +0700

    Add push notification

---
+++ app/src/main/java/com/teguhrmdhn/movapp/ComingSoonPageActivity.kt
@@ -0,0 +1,11 @@
+package com.teguhrmdhn.movapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComingSoonPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coming_soon_page)
+    }
+}
--- app/src/main/java/com/teguhrmdhn/movapp/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/teguhrmdhn/movapp/checkout/CheckoutActivity.kt
@@ -1,8 +1,15 @@
 package com.teguhrmdhn.movapp.checkout
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.teguhrmdhn.movapp.R
 import com.teguhrmdhn.movapp.model.Checkout
@@ -35,6 +42,45 @@ class CheckoutActivity : AppCompatActivity() {
         btn_checkout.setOnClickListener {
             var intent = Intent(this, CheckoutSuccessActivity::class.java)
             startActivity(intent)
+
+            showNotif()
         }
+    }
+
+    private fun showNotif() {
+        val NOTIFICATION_CHANNEL_ID = "Channel_bwa_notif"
+        val context = this.applicationContext
+        var notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channelName = "MOV APP Notif Channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val mChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val mIntent = Intent(this, CheckoutSuccessActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("id", "id_film")
+        mIntent.putExtras(bundle)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        builder.setContentIntent(pendingIntent)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher))
+
+            .setTicker("Notif MOV starting")
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000,1000))
+            .setLights(Color.RED, 3000, 3000)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setContentTitle("Payment Successful")
+            .setContentText("Your Payment has been success")
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(115, builder.build())
     }
 }
